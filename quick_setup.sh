7d7018af47d5a5d61685a72ecf53953d9a32cc97
Ouhfi <ouhfid4@gmail.com>
2026-07-14 11:49:49 +0100

    quick setup fail

---
+++ quick_setup.sh
@@ -0,0 +1,23 @@
+# Initialize project
+npm init -y
+
+# Production dependencies
+npm install \
+express \
+sequelize \
+pg \
+pg-hstore \
+dotenv \
+bcrypt \
+jsonwebtoken \
+joi \
+cors \
+helmet \
+morgan \
+express-async-errors \
+@google/genai
+
+# Development dependencies
+npm install -D \
+nodemon \
+sequelize-cli
